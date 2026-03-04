606b69a4f4163c901427fd11c44bee52d89c3de7
Artur Gonçalves <arturvicente.goncalves@gmail.com>
2026-03-04 11:40:54 +0000

    - Changed all commands to /commands for ease of use - Removed create thread command because it no longer makes sense - Updated README

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Functions:
 - Asks questions from the file "question_file.txt" and opens public threads for discussions and voting
 - Set target channel to avoid direct questions there
 - Command based activities
-- Several debug commands for functionallity checks
+- Several debug commands for functionallity checks (Although some will not respond directly to the commands and instead just send a message due to the nature of the functions they check)
 - Automatic daily questions (by default set to midnight, manageable via commands)
 - Target channel, thread and time for automatic questions saved between runs of the bot
 - Exclusion of questions that have already been asked
@@ -14,12 +14,13 @@ HOW TO USE:
 1. Create a file named "token_file.txt" and paste in your discord application token
 2. If you wish to use the automatic questions:
     - Add your questions to the file "question_file.txt", one per line
-    - Use the command $SphinxieSetChannel in a channel to set that as the channel where questions will appear
-    - Use the command $SphinxieSetQuestionTime hh mm to set the time at which questions are sent otherwise it will default to midnight in local time
+    - Use the command /set_channel in a channel to set that as the channel where questions will appear
+    - Use the command /set_question_time hh mm to set the time at which questions are sent otherwise it will default to midnight in local time
 
 Planned Features:
 - A list of questions included with the program itself
 - Poll summary at the end of every day (current rankings)
 - Questions that target specific users/have options of types other than the users
 - Preventing users from voting more than once
-- Preventing crashing on votes after restart and before asking question
+- Preventing crashing on votes after restart and before asking question/restoring votes from previous session on boot
+- Joining of saved data for target channels and times in one file
--- Sphinxie.py
+++ Sphinxie.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import tasks
 import datetime
 import asyncio
@@ -12,6 +13,7 @@ intents.members = True
 
 # Defines the client so the bot can log in and do things
 client = discord.Client(intents=intents)
+client.tree = app_commands.CommandTree(client)
 
 # Finds out the local time zone to be used when setting times
 local_tz = datetime.datetime.now().astimezone().tzinfo
@@ -82,93 +84,107 @@ async def on_ready():
     if not daily_question.is_running():
             daily_question.start()
 
+    await app_commands.CommandTree.sync(client.tree)
+
     print(f"Successfully logged in as {client.user}.")
 
-# When the user sends a command in a message on discord, reads it then acts accordingly
-@client.event
-async def on_message(message):
+@client.tree.command(name = "set_channel", description = "Sets current channel as target.")
+async def setchannel(interaction: discord.Interaction):
     global targetChannel
-    global targetThread
-    if message.author == client.user:
-        return
+    channel = interaction.channel.id
+    with open("channel_file.txt", "w", encoding="UTF-8") as channel_file:
+        channel_file.write(str(channel))
+    targetChannel = client.get_channel(channel)
+    await interaction.response.send_message("Target channel set!")
 
-    if message.content.startswith("$SphinxieSetChannel"):
-        channel = message.channel.id
-        with open("channel_file.txt", "w", encoding="UTF-8") as channel_file:
-            channel_file.write(str(channel))
-        targetChannel = client.get_channel(channel)
-        await targetChannel.send("Target channel set!")
+@client.tree.command(name = "ask_question", description = "Asks question in target channel or here if none is set.")
+async def ask_question(interaction: discord.Interaction):
+    if targetChannel == None:
+        await ask_question(interaction.channel)
+    else: await ask_question(targetChannel)
 
-    if message.content.startswith("$SphinxieAskQuestion"):
-        if targetChannel == None:
-            await ask_question(message.channel)
-        else: await ask_question(targetChannel)
+@client.tree.command(name = "set_question_time", description = "Sets the target time for the daily question.")
+@app_commands.describe(hour = "The hour at which to send.", minute = "The minute at which to send.")
+async def set_question_time(interaction: discord.Interaction, hour: int, minute: int):
+    if not setTargetTime(hour, minute):
+        await interaction.response.send_message("Invalid time given, no changes were made.")
+    else:
+        await interaction.response.send_message("Target time set to "+targetTime.strftime("%H:%M")+".")
 
-    if message.content.startswith("$SphinxieSetQuestionTime"):
-        if not setTargetTime(message.content):
-            await message.channel.send("Target time not in valid format, \
-                                       please use the command with the following format: \
-                                       $SphinxieSetQuestionTime hh mm.")
-        else:
-            daily_question.change_interval(time=targetTime)
-            await message.channel.send("Target time set to "+targetTime.strftime("%H:%M")+".")
-
-    if message.content.startswith("$SphinxieEnableDailyQuestion"):
-        if not daily_question.is_running():
-            daily_question.start()
-            await message.channel.send("Daily question enabled.")
-        else:
-            await message.channel.send("Daily question is already on.")
+@client.tree.command(name = "enable_daily_question", description = "Makes Sphinxie ask questions at the specified time.")
+async def enable_daily_question(interaction: discord.Interaction):
+    if not daily_question.is_running():
+        daily_question.start()
+        await interaction.response.send_message("Daily question enabled.")
+    else:
+        await interaction.response.send_message("Daily question is already on.")
     
-    if message.content.startswith("$SphinxieDisableDailyQuestion"):
-        if daily_question.is_running():
-            daily_question.cancel()
-            await message.channel.send("Daily question disabled.")
-        else:
-            await message.channel.send("Daily question is already off.")
+@client.tree.command(name = "disable_daily_question", description = "Makes Sphinxie stop asking questions at the specified time.")
+async def disable_daily_question(interaction: discord.Interaction):
+    if daily_question.is_running():
+        daily_question.cancel()
+        await interaction.response.send_message("Daily question disabled.")
+    else:
+        await interaction.response.send_message("Daily question is already off.")
 
-    if message.content.startswith("$SphinxiePresenceCheck"):
-        await message.channel.send('Hello world!')
-    
-    if message.content.startswith("$SphinxieCheckUserList"):
-        await message.channel.send(get_members())
+@client.tree.command(name = "presence_check", description = "Easy way to check if Sphinxie is currently up and running.")
+async def presence_check(interaction: discord.Interaction):
+    await interaction.response.send_message('Hello world!')
 
-    if message.content.startswith("$SphinxieCheckQuestionTime"):
-        await message.channel.send("The daily question is set to "+targetTime.strftime("%H:%M")+".")
+@client.tree.command(name = "check_user_list", description = "Sends a list of all users currently detected.")
+async def check_user_list(interaction: discord.Interaction):
+    await interaction.response.send_message(get_members())
 
-    if message.content.startswith("$SphinxieUnsetChannel"):
-        if targetChannel == None:
-            await message.channel.send("There is no targeted channel.")
-        else:
-            targetChannel = None
-            await message.channel.send("Target channel unset!")
+@client.tree.command(name = "check_question_time", description = "Shows which time the daily question is set for.")
+async def check_question_time(interaction: discord.Interaction):
+    await interaction.response.send_message("The daily question is set to "+targetTime.strftime("%H:%M")+".")
 
-    if message.content.startswith("$SphinxieUnsetThread"):
-        if targetThread == None:
-            await message.channel.send("There is no targeted thread.")
-        else:
-            targetThread = None
-            await message.channel.send("Target thread unset!")
-        
-    if message.content.startswith("$SphinxieMakePoll"):
-        options = get_members()
-        await make_poll(message.channel, get_question(), options)
-    
-    if message.content.startswith("$SphinxieCreateThread"):
-        await open_thread(message, "This is a thread.")
-
-    if message.content.startswith("$SphinxieClearConsumedQuestions"):
-        with open("consumed_file.txt", "w", encoding="UTF-8") as _:
+@client.tree.command(name = "unset_channel", description = "Removes the target channel, leaving it unset.")
+async def unset_channel(interaction: discord.Interaction):
+    global targetChannel
+    if targetChannel == None:
+        await interaction.response.send_message("There is no targeted channel.")
+    else:
+        targetChannel = None
+        with open("channel_file.txt", "w", encoding="UTF-8") as _:
             pass
-        await message.channel.send("Consumed questions cleared!")
+        await interaction.response.send_message("Target channel unset!")
 
-    if message.content.startswith("$SphinxieVote"):
-        voteResult = vote(message.content)
+@client.tree.command(name = "unset_thread", description = "Removes the target thread, leaving it unset.")
+async def unset_thread(interaction: discord.Interaction):
+    global targetThread
+    if targetThread == None:
+        await interaction.response.send_message("There is no targeted thread.")
+    else:
+        targetThread = None
+        with open("thread_file.txt", "w", encoding="UTF-8") as _:
+            pass
+        await interaction.response.send_message("Target thread unset!")
+        
+@client.tree.command(name = "make_poll", description = "Begins a poll in the channel where the command was used.")
+async def make_poll(interaction: discord.Interaction):
+    options = get_members()
+    await make_poll(interaction.channel, get_question(), options)
+
+@client.tree.command(name = "clear_consumed_questions", description = "Resets the list of consumed questions.")
+async def clear_consumed_questions(interaction: discord.Interaction):
+    with open("consumed_file.txt", "w", encoding="UTF-8") as _:
+        pass
+    await interaction.response.send_message("Consumed questions cleared!")
+
+@client.tree.command(name = "vote", description = "Votes for specified user.")
+@app_commands.describe(user = "The number of the user to vote for.")
+async def vote(interaction: discord.Interaction, user: int):
+    voteResult = vote(user)
+    if voteResult == None:
+        await interaction.response.send_message("Voting unsuccessful, invalid user chosen.")
+    else:
         users = get_members()
-        await message.channel.send("Successfully voted for "+users[voteResult]+"!")
+        await interaction.response.send_message("Successfully voted for "+users[voteResult]+"!")
 
-    if message.content.startswith("$SphinxieFinishVoting"):
-        await message.channel.send(makeSummary())
+@client.tree.command(name = "finish_voting", description = "Immediately finishes voting and sends results.")
+async def finish_voting(interaction: discord.Interaction):
+    await interaction.response.send_message(makeSummary())
 
 async def ask_question(location):
     """
@@ -220,6 +236,7 @@ async def make_poll(location, question, options):
             poll+=(str(n+1)+"- "+options[n]+"\n")
             n+=1
         message = await location.send(poll)
+        initializeVoting()
         return message
 
 def get_question():
@@ -271,7 +288,7 @@ def consume_question(question):
 
 def get_members():
     """
-    Returns a list with the display names of all members of the server (excluding bots).
+    Returns a list with the display names of all members of the server (excluding bots) + a "no one".
     """
     unsanitizedMembers = client.get_all_members()
     members = []
@@ -288,15 +305,10 @@ def initializeVoting():
     while len(givenVotes) < amount:
         givenVotes.append(0)
 
-def vote(message):
+def vote(target):
     global givenVotes
-    content = message.split()
-    try:
-        target = int(content[1])
-    except ValueError:
-        return False
-    except IndexError:
-        return False
+    if not (len(givenVotes) >= target >= 1):
+        return None
     givenVotes[target-1]+=1
     return target-1
 
@@ -323,24 +335,19 @@ def makeSummary():
             winnerString += users[winners[n]]+", "
         return "The winners are: "+winnerString+"with "+str(highest)+" votes each!"
 
-def setTargetTime(message):
+def setTargetTime(hour, minute):
     """
     Sets the time at which the daily question should be asked to the specified one.
     
-    :param message: Message with the command to set the time (string)
+    :param hour: The hour at which to ask the question.
+    :param minute: The minute at which to ask the question.
     """
     global targetTime
-    content = message.split()
-    try:
-        hour = int(content[1])
-        minute = int(content[2])
-    except ValueError:
-        return False
-    except IndexError:
+    if not (23 >= hour >= 0 and 59 >= minute >= 0):
         return False
     targetTime = datetime.time(hour=hour, minute=minute, tzinfo=local_tz)
     with open("time_file.txt", "w", encoding="UTF-8") as time_file:
-            time_file.writelines(content[1]+"\n"+content[2])
+            time_file.writelines(str(hour)+"\n"+str(minute))
     return True
 
 # Obtains the bot's login token from the token_file
